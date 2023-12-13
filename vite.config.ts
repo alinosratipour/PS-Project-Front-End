b8db2eb78730e781bab0ca7196ba43eab18da962
ali <alinosratipour@gmail.com>
2023-12-13 12:29:10 +0000

    clean code

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ViteSassPlugin from "vite-plugin-sass"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
         ],
       },
     }),
+    ViteSassPlugin(),
   ],
 
   server: {
