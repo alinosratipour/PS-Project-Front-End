446ec666f2b561399b991ac8fa4fcd77d2d18b7b
ali <alinosratipour@gmail.com>
2023-12-21 17:46:23 +0000

    tweak

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-// Wrap your top-level component (e.g., App) with AvailableBasesProvider
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import App from "./App";
 import apolloClient from "./apolloClient";
-import ContextProvider from './components/Context/ContextProvider';
+import ContextProvider from "./components/Context/ContextProvider";
 import "./globalStyles.scss";
-ReactDOM.render(
-  <ApolloProvider client={apolloClient}>
-  <ContextProvider>
-    <App />
-  </ContextProvider>
-</ApolloProvider>,
-  document.getElementById("root")
-);
+
+const root = document.getElementById("root");
+
+if (root) {
+  const reactRoot = createRoot(root);
+  reactRoot.render(
+    <ApolloProvider client={apolloClient}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </ApolloProvider>
+  );
+}
+// ReactDOM.render(
+
+//   document.getElementById("root")
+// );
